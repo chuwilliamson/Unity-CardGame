a2a24d3082b10f28594c12cc6130ab61cf7538a5
Matthew Williamson <chuwilliamson@gmail.com>
2019-08-10 13:56:49 -0500

    add project folders and scripts
    Signed-off-by: Matthew Williamson <chuwilliamson@gmail.com>

---
+++ Assets/Scripts/TextVariable.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+
+public class TextVariable : INotifyPropertyChanged
+{
+    private string _value;
+
+    public string Value
+    {
+        get => _value;
+        set
+        {
+            _value = value;
+            OnPropertyChanged(_value);
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    [NotifyPropertyChangedInvocator]
+    private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
